d2bc6a1bdc82c72b3ec284d7c5e97e91b4ba2d37
OscGitHub123 <92007808+OscGitHub123@users.noreply.github.com>
2023-07-03 21:57:21 +0800

    Update script.js

--- My Coding Activity/Web iseng/script.js
+++ My Coding Activity/Web iseng/script.js
@@ -42,37 +42,57 @@ var countdownDate = new Date("August 9, 2023 16:00:00").getTime();
 
 
 
-document.addEventListener('DOMContentLoaded', function() {
-  // Mengambil elemen-elemen form
-  const form = document.getElementById('form-rsvp');
-  const namaInput = document.getElementById('input-nama');
-  const ucapanInput = document.getElementById('input-ucapan');
-  const kontainerPesan = document.getElementById('pesan-chat');
 
-  // Menambahkan event listener pada form ketika dikirim
-  form.addEventListener('submit', function(event) {
-    event.preventDefault(); // Mencegah form submit
+    var messages = []; // Array untuk menyimpan pesan-pesan
+var currentSlideIndex = 0; // Indeks slide saat ini
 
-    const nama = namaInput.value;
-    const ucapan = ucapanInput.value;
+document.getElementById("rsvp-form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Mencegah pengiriman formulir secara default
 
-    // Membuat elemen untuk pesan output
-    const pesan = document.createElement('div');
-    pesan.classList.add('pesan', 'pesan-output');
-    const balonPesan = document.createElement('div');
-    balonPesan.classList.add('balon-pesan');
-    balonPesan.classList.add('float-left')
-    const teksPesan = document.createElement('p');
-    teksPesan.classList.add('teks-pesan');
-    teksPesan.innerHTML = `<strong>${nama}</strong>: ${ucapan}`;
+  // Mengambil nilai-nilai input dari formulir
+  var name = document.getElementById("name").value;
+  var message = document.getElementById("message").value;
+  var attendance = document.getElementById("attendance").value;
 
-    // Menambahkan pesan output ke dalam kontainer pesan
-    balonPesan.appendChild(teksPesan);
-    pesan.appendChild(balonPesan);
-    kontainerPesan.appendChild(pesan);
+  // Membuat objek pesan
+  var pesan = {
+    name: name,
+    message: message,
+    attendance: attendance
+  };
 
-    // Mengosongkan field input setelah dikirim
-    namaInput.value = '';
-    ucapanInput.value = '';
+  // Menyimpan pesan dalam array
+  messages.push(pesan);
+
+  // Menampilkan output dalam output-slides
+  var outputSlides = document.getElementById("output-slides");
+  outputSlides.innerHTML = "";
+
+  messages.forEach(function(item) {
+    var outputElement = document.createElement("div");
+    outputElement.className = "output-message";
+    outputElement.innerHTML = "<h2>Terima kasih, " + item.name + "!</h2>" +
+                              "<p>Ucapan Anda: " + item.message + "</p>" +
+                              "<p>Kehadiran: " + item.attendance + "</p>";
+    outputSlides.appendChild(outputElement);
   });
+
+  // Reset formulir
+  document.getElementById("rsvp-form").reset();
+
+  // Mengatur indeks slide saat ini ke slide terakhir
+  currentSlideIndex = messages.length - 1;
+});
+
+// Slide berikutnya
+var nextButton = document.getElementById("next-button");
+nextButton.addEventListener("click", function() {
+  var outputSlides = document.getElementById("output-slides");
+  var slideHeight = outputSlides.clientHeight;
+
+  // Menggeser slide hanya jika masih ada slide berikutnya
+  if (currentSlideIndex < messages.length - 1) {
+    currentSlideIndex++;
+    outputSlides.style.transform = "translateY(-" + (slideHeight * currentSlideIndex) + "px)";
+  }
 });
